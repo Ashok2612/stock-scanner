642546d51dfdaa073bf48e3ff9084b66cfc289dc
Ashok2612 <115620802+Ashok2612@users.noreply.github.com>
2025-12-09 11:12:36 +0530

    Update server.py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ CORS(app)
 # ==============================================================================
 client_id = "J3PIUDQS20-100"      # <--- PASTE YOUR ID HERE
 secret_key = "8TM69GG411"       # <--- PASTE YOUR SECRET HERE
-redirect_uri = "http://127.0.0.1:5000/login"
+redirect_uri = "https://stock-scanner-vcza.onrender.com"
 # ==============================================================================
 
 access_token = None
@@ -136,4 +136,5 @@ def get_stock():
 
 if __name__ == '__main__':
     webbrowser.open("http://127.0.0.1:5000/login")
+
     app.run(port=5000)
