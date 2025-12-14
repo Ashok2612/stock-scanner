5819c933669b801ca632e79bf527ddd00d391af7
SwingTrader <trader@example.com>
2025-12-14 02:48:53 +0000

    Final cleanup of server files.

--- server.py
+++ server.py
@@ -1,5 +1,9 @@
+# server.py - Cleaned Code
+
 from flask import Flask, render_template
 import os
+# The imports for request and jsonify (and fyers_data_api) have been removed
+# because the chart_data route is being removed.
 
 app = Flask(__name__)
 
@@ -8,32 +12,11 @@ def home():
     # This serves the Dashboard file we created
     return render_template('index.html')
 
+# --- Fyers API Code Removed ---
+# The following code block was removed to stop the server crash:
+# from fyers_data_api import fetch_and_format_chart_data 
+# @app.route('/api/chart_data')... (entire chart_data function)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-# server.py (Additions)
-
-# --- Add this line near the top with your other imports ---
-from fyers_data_api import fetch_and_format_chart_data 
-
-# ... (rest of your existing code) ...
-
-# --- Add this new route, typically near the end of your file ---
-@app.route('/api/chart_data')
-def chart_data():
-    """Endpoint to serve chart data to the frontend from Fyers API."""
-    symbol = request.args.get('symbol', 'FOCUS') 
-
-    # 1. Fetch data using the new module
-    data = fetch_and_format_chart_data(symbol)
-    
-    # 2. Return the data as JSON
-    if data:
-        return jsonify(data)
-    else:
-        # Return a 500 error if data fetching failed
-        return jsonify([]), 500
-
-# ... (the rest of your server.py) ...
